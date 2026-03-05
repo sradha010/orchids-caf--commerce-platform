2d99e7ba184c7d95e731f48c42b83ef99b754a3a
sradha010 <156775929+sradha010@users.noreply.github.com>
2026-03-05 21:17:17 +0530

    Update hero images in Hero.tsx

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,8 +7,8 @@ import { SITE_CONFIG } from "@/constants";
 // Add your image paths here
 const HERO_IMAGES = [
   "/hero.avif",
-  "/coffee_interior.webp", // Add your second image path
-  "/coffee_interior2.webp",   // Add your third image path
+  "/coffee2.avif", // Add your second image path
+  "/coffee3.avif",   // Add your third image path
 ];
 
 export function Hero() {
